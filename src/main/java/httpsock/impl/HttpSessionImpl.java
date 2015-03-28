f5773163da2d4f8a86f7877d6e8d5025b85e3e46
yoo <lukas@yooui-MacBook-Pro.local>
2015-03-28 22:15:14 +0900

    세션 수신 메세지 디코더 및 CharBuffer 래핑

--- src/main/java/httpsock/impl/HttpSessionImpl.java
+++ src/main/java/httpsock/impl/HttpSessionImpl.java
@@ -12,6 +12,7 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 
 /**
@@ -20,11 +21,13 @@ import java.nio.charset.CharsetEncoder;
 public class HttpSessionImpl implements HttpSession {
     private final Logger logger = LoggerFactory.getLogger(HttpSessionImpl.class);
 
+    protected final CharsetDecoder decoder;
     protected final CharsetEncoder encoder;
     protected final SocketChannel socketChannel;
     protected final ByteBuffer byteBuffer;
 
     public HttpSessionImpl(SocketChannel socketChannel) {
+        this.decoder = Charset.forName("UTF-8").newDecoder();
         this.encoder = Charset.forName("UTF-8").newEncoder();
         this.socketChannel = socketChannel;
         this.byteBuffer = ByteBuffer.allocate(4096);
@@ -38,13 +41,16 @@ public class HttpSessionImpl implements HttpSession {
             byteBuffer.limit(byteBuffer.capacity());
             Preconditions.checkState(socketChannel.read(byteBuffer) > 0, "socketChannel's stream is end");
             byteBuffer.flip();
-            byteBuffer.position();
 
             StringBuffer internalBuffer = new StringBuffer();
-
-            while (byteBuffer.hasRemaining()) {
-                internalBuffer.append(byteBuffer.getChar());
+            CharBuffer readCharBuffer = decoder.decode(byteBuffer);
+            while (readCharBuffer.hasRemaining()) {
+                internalBuffer.append(readCharBuffer.get());
             }
+
+            byteBuffer.clear();
+
+            logger.info("[" + getClass().getSimpleName() + "] : received data\n {}", internalBuffer.toString());
 
             httpRequest = new HttpRequestImpl(internalBuffer.toString().trim());
         } catch (NullPointerException ex) {
@@ -69,6 +75,8 @@ public class HttpSessionImpl implements HttpSession {
             try {
                 socketChannel.write(encoder.encode(CharBuffer.wrap(rawResponse)));
                 socketChannel.close();
+
+                logger.info("[" + getClass().getSimpleName() + "] : sended data\n {}", rawResponse);
             } catch (IOException ex) {
                 logger.error("[" + getClass().getSimpleName() + "] : {}", ex.toString());
             }
